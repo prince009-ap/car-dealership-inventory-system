b6f19ef6e531587b62a0f373b6ae00724d6a8b19
prince009-ap <senjaliyaprince009@gmail.com>
2026-07-12 10:46:22 +0530

    chore: production configuration

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://car-dealership-inventory-system-chi.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
   })
